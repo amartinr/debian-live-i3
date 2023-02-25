9a31cf199d248ba6290af4a807589e22df52a435
amartinr <amartinr@kotetsu>
2023-02-25 18:27:02 +0100

    Minor fixes to improve readability

--- testlive.sh
+++ testlive.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
-if [ -z "$1" ]; then
+function usage() {
     echo "Usage: $(basename $0) <systemd|systemd-ro|qemu|xserver>"
     exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
 fi
 
 case "$1" in
     systemd)
-        sudo systemd-nspawn -a -D squashfs-root -M debian-live-11 --setenv=DISPLAY=:0 --setenv=SHELL=/bin/bash
-        #sudo systemd-nspawn -D squashfs-root -M debian-live-11 --setenv=DISPLAY=:0 --setenv=SHELL=/bin/bash --user=root /bin/bash
+        sudo systemd-nspawn -b -D squashfs-root -M debian-live-11 \
+            --setenv=DISPLAY=:0 --setenv=SHELL=/bin/bash
     ;;
     systemd-ro)
-        sudo systemd-nspawn -a -D squashfs-root -M debian-live-11 --setenv=DISPLAY=:0 --setenv=SHELL=/bin/bash --read-only --user=root /bin/bash
+        sudo systemd-nspawn -b -D squashfs-root -M debian-live-11 \
+            --setenv=DISPLAY=:0 --setenv=SHELL=/bin/bash --read-only 
     ;;
     qemu)
-        qemu-system-x86_64 -enable-kvm -m 4G -cdrom debian-live-11.6.0-amd64-custom+nonfree.iso -display gtk,show-cursor=on -vga virtio
+        qemu-system-x86_64 -enable-kvm -m 4G \
+            -cdrom debian-live-11.6.0-amd64-custom+nonfree.iso \
+            -display gtk,show-cursor=on -vga virtio
     ;;
     xserver)
-        Xephyr -ac -screen 1360x768 -resizeable -reset :1 & sleep 1
-        sudo systemd-nspawn -D squashfs-root -M debian-live-11 --setenv=DISPLAY=:1 --setenv=SHELL=/bin/bash --user=root i3
-        sleep 2
+        Xephyr -ac -screen 1360x768 -resizeable -reset :1 & \
+        sudo systemd-nspawn -a -D squashfs-root -M debian-live-11 \
+            --setenv=DISPLAY=:1.0 --setenv=SHELL=/bin/bash --user=user /etc/X11/Xsession
         pkill Xephyr
     ;;
     *)
-        echo "Usage: $(basename $0) <systemd|systemd-ro|qemu|xserver>"
-        exit 1
+        usage
     ;; 
 esac
